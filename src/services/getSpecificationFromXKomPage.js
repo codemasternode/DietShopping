4693b33a2104660d242f323ff17728aabefdb719
codemasternode <marcinwarzybok@outlook.com>
2020-07-24 22:53:15 +0200

    prepare scripts for get info from x-kom page

---
+++ src/services/getSpecificationFromXKomPage.js
@@ -0,0 +1,25 @@
+function getSpecificationFromXKomPage() {
+    const fullSpecification = []
+    const specificationList = document.querySelectorAll(".sc-13p5mv-0")
+    console.log(specificationList)
+    for (let i = 0; i < specificationList.length; i++) {
+        console.log(specificationList[i].querySelectorAll("div")[2])
+        fullSpecification.push({
+            parameter: specificationList[i].querySelector("div").querySelector("div").innerText.trim(),
+            value: specificationList[i].querySelectorAll("div")[2].querySelector("div").innerText.trim()
+        })
+    }
+    console.log(JSON.stringify(fullSpecification, undefined, 5))
+}
+
+function getBulletPoints() {
+    const bulletpoints = []
+    const list = document.querySelector(".jSFGJL").querySelectorAll("li")
+    for (let i = 0; i < list.length; i++) {
+        bulletpoints.push({
+            parameter: list[i].querySelector("span").innerText.trim().split(":")[0],
+            value: list[i].querySelectorAll("span")[1].innerText.trim()
+        })
+    }
+    console.log(JSON.stringify(bulletpoints, undefined, 5))
+}
--- test/routes/productsRoutes.spec.js
+++ test/routes/productsRoutes.spec.js
@@ -5,19 +5,63 @@ import uniqid from 'uniqid'
 import chai from 'chai'
 import chaiHttp from 'chai-http'
 import server from '../../src/index'
+import chaiExpectedCookie from 'chai-expected-cookie'
+
+let should = chai.should();
+
+chai.use(chaiHttp)
+chai.use(chaiExpectedCookie)
 
 describe("test /api/products/get-products-by-page", () => {
     it("should send array of products", (done) => {
-        
+        chai.request(server)
+            .post("/api/products/get-products-by-page")
+            .send({
+                page: 1,
+                sort: "created_at"
+            }).end((err, res) => {
+                res.should.have.status(200);
+                res.should.have.property("products")
+                expect(res.body.products).to.be.an("array")
+                done()
+            })
     })
     it("should send array of products paginated on first page", (done) => {
-
+        chai.request(server)
+            .post("/api/products/get-products-by-page")
+            .send({
+                page: 1,
+                sort: "created_at"
+            }).end((err, res) => {
+                res.should.have.status(200);
+                res.should.have.property("products")
+                expect(res.body.products).to.be.an("array")
+                done()
+            })
     })
     it("should send array of products paginated on third page", (done) => {
-
+        chai.request(server)
+            .post("/api/products/get-products-by-page")
+            .send({
+                page: 1,
+                sort: "created_at"
+            }).end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.have.property("products")
+                expect(res.body.products).to.be.an("array")
+                done()
+            })
     })
     it("should return 404 on paginated page out of range", (done) => {
-
+        chai.request(server)
+            .post("/api/products/get-products-by-page")
+            .send({
+                page: 200,
+                sort: "created_at"
+            }).end((err, res) => {
+                res.should.have.status(404);
+                done()
+            })
     })
 })
 
